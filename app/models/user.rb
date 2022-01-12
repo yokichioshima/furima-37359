18206f83ad089c03eb741657fe977a02a8ed0a48
yokichioshima <96287511+yokichioshima@users.noreply.github.com>
2022-01-12 15:16:41 +0900

    新規登録機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
-  validates :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana,
-            :birth_date, presence: true
+  validates :nickname, :birth_date, presence: true
+  validates :last_name, :first_name,
+              presence: true,
+              format: { 
+                     with: /\A[ぁ-んァ-ン一-龥]+\z/,
+                     message: 'に全角文字を使用してください' 
+              } 
+  validates :last_name_kana, :first_name_kana, 
+              presence: true,
+              format: {
+                     with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/,
+                     message: "全角カタカナのみで入力して下さい"
+               }
 
 end
