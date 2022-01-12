b237e9d6fd68a124728a561fb298918785a936a4
yokichioshima <96287511+yokichioshima@users.noreply.github.com>
2022-01-12 23:17:30 +0900

    BlankCeckSupport実装準備

---
+++ spec/support/blank_check_support.rb
@@ -0,0 +1,7 @@
+module BlankCheckSupport
+  def blank_check(object, key)
+    @object.key = ''
+    @object.valid?
+    expect(@object.errors.full_messages).to include("#{object.capitalize.gsub(/_/,' ')} can't be blank")
+  end
+end
