87b6f44ad1d043d49570afd690a853355ade578a
yokichioshima <96287511+yokichioshima@users.noreply.github.com>
2022-01-13 15:09:49 +0900

    rubocop実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, :birth_date, presence: true
-  validates :password, 
+  validates :password,
             presence: true,
             format: {
               with: /\A(?=.*?[a-z])(?=.*?[0-9])[a-z0-9]+\z/,
