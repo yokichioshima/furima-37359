6db88978796e43f66a5e61279f59b3a5357cf86a
yokichioshima <96287511+yokichioshima@users.noreply.github.com>
2022-01-21 22:10:54 +0900

    items.controller修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,7 +24,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    redirect_to root_path if @item.order.nil?
+    redirect_to root_path if @item.order != nil
   end
 
   def update
